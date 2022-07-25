48319fa5abeaa1a01ab68cfbca9702c96c765428
tougherr <17394529685@163.com>
2022-07-25 14:05:12 +0800

    navie birthdat attack

---
+++ naive birthday attack/naive birthday attack/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <openssl/sm3.h>
+#include <openssl/evp.h>
+
+using namespace std;
+
+
+void print_dgst(const uint8_t str[SM3_DIGEST_LENGTH]) {
+	for (int i = 0; i < SM3_DIGEST_LENGTH; i++) {
+		printf("%x", str[i]);
+	}
+	printf("\n");
+}
+
+int main(int argc, char** argv) {
+
+	char plain[] = "birthday attack";
+	int plain_len = strlen(plain);
+
+	uint8_t dgst[SM3_DIGEST_LENGTH];
+	unsigned int dgst_len = 0;
+
+	EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+	EVP_DigestInit_ex(ctx, EVP_sm3(), NULL);
+	EVP_DigestUpdate(ctx, plain, plain_len);
+	EVP_DigestFinal(ctx, dgst, &dgst_len);
+
+	printf("message digest: ");
+	print_dgst(dgst);
+	EVP_MD_CTX_free(ctx);
+
+	uint8_t attack[32] = { 0 };
+	uint8_t dgst1[SM3_DIGEST_LENGTH];
+	unsigned int dgst_len1 = 0;
+
+	for (int i = 0; ; i++) {
+		ctx = EVP_MD_CTX_new();
+		EVP_DigestInit_ex(ctx, EVP_sm3(), NULL);
+		EVP_DigestUpdate(ctx, attack, 32);
+		EVP_DigestFinal(ctx, dgst1, &dgst_len1);
+		EVP_MD_CTX_free(ctx);
+		int flag = memcmp(dgst, dgst1, 3);
+		if (flag == 0) {
+			printf("collision digest: ");
+			print_dgst(dgst1);
+			break;
+		}
+		*((uint64_t*)attack) += 1;
+	}
+
+	return 0;
+}
